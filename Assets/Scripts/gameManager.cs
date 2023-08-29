466a3ce63c49912b819c647293518e071c37a2a2
Ваше имя <zaazaa@yandex.ru>
2023-08-29 19:06:17 +0300

    Игра Memory на основе чужой разработки. Добавил Grid и свои элементы

---
+++ Assets/Scripts/gameManager.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+public class gameManager : MonoBehaviour {
+
+	public Sprite[] cardFace;
+	public Sprite cardBack;
+	public GameObject[] cards;
+	public GameObject gameTime;
+
+	private bool _init  = false;
+	private int _matches = 4;
+
+	// Update is called once per frame
+	void Update () {
+		if (!_init)
+			initializeCards ();
+
+		if (Input.GetMouseButtonUp (0))
+			checkCards ();
+
+	}
+
+	void initializeCards() {
+
+		List<int> cardValues = new List<int> (cardFace.Length/2);
+		for(int i=0;i<cardFace.Length; i++) { cardValues.Add (i+1); cardValues.Add(i+1); }
+        foreach (var card in cards)
+        {
+			int choice=Random.Range(0, cardValues.Count);
+			card.GetComponent<cardScript>().cardValue = cardValues[choice];
+            card.GetComponent<cardScript>().initialized = true;
+            cardValues.RemoveAt(choice);
+        }
+
+        /*
+		for (int id = 0; id < 2; id++) {
+			for (int i = 1; i <=cardFace.Length/2; i++) {
+
+				bool test = false;
+				int choice = 0;
+				while (!test) {
+					choice = Random.Range (0, cards.Length);
+					test = !(cards [choice].GetComponent<cardScript> ().initialized);
+				}
+				cards [choice].GetComponent<cardScript> ().cardValue = i;
+				cards [choice].GetComponent<cardScript> ().initialized = true;
+			}
+		}*/
+
+        foreach (GameObject c in cards)
+			c.GetComponent<cardScript> ().setupGraphics ();
+
+		if (!_init)
+			_init = true;
+	}
+
+	public Sprite getCardBack() {
+		return cardBack;
+	}
+
+	public Sprite getCardFace(int i) {
+		return cardFace[i - 1];
+	}
+
+	void checkCards() {
+		List<int> c = new List<int> ();
+
+		for (int i = 0; i < cards.Length; i++) {
+			if (cards [i].GetComponent<cardScript> ().state == 1)
+				c.Add (i);
+		}
+
+		if (c.Count == 2)
+			cardComparison (c);
+	}
+
+	void cardComparison(List<int> c){
+		cardScript.DO_NOT = true;
+
+		int x = 0;
+
+		if (cards [c [0]].GetComponent<cardScript> ().cardValue == cards [c [1]].GetComponent<cardScript> ().cardValue) {
+			x = 2;
+			_matches--;
+			if (_matches == 0)
+				gameTime.GetComponent<timeScript> ().endGame ();
+		}
+
+
+		for (int i = 0; i < c.Count; i++) {
+			cards [c [i]].GetComponent<cardScript> ().state = x;
+			cards [c [i]].GetComponent<cardScript> ().falseCheck ();
+		}
+	
+	}
+
+	public void reGame(){
+		SceneManager.LoadScene ("gameScene");
+	}
+
+	public void reMenu(){
+		SceneManager.LoadScene ("menuScene");
+	}
+}
